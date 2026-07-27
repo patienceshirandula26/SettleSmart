56eccd34b0e3cd52ce80e120f1201a627a719674
patiencenjeri99 <patiencenjeri.shirandula@my.jcu.edu.au>
2026-07-28 00:55:49 +1000

    Connect Flask to MySQL and add users API

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pymysql
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+def get_connection():
+    return pymysql.connect(
+        host=app.config["MYSQL_HOST"],
+        user=app.config["MYSQL_USER"],
+        password=app.config["MYSQL_PASSWORD"],
+        database=app.config["MYSQL_DB"],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
 
 @app.route("/")
 def home():
     return "Welcome to SettleSmart Backend!"
+
+
+@app.route("/api/users")
+def get_users():
+    connection = get_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+    SELECT user_id, full_name, email, university, arrival_date, created_at
+    FROM users
+""")
+            users = cursor.fetchall()
+
+        return jsonify(users)
+
+    finally:
+        connection.close()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
