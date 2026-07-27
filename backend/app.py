8d5b978bb496c44202c296392abe5f6115991d2e
patiencenjeri99 <patiencenjeri.shirandula@my.jcu.edu.au>
2026-07-28 01:54:29 +1000

    Add login authentication between frontend and backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import pymysql
 from config import Config
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
 
